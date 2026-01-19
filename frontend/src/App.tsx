fda4a01513249f308ace1d34c4a668bf102e7dbf
jruifri583 <jruifri583@g.educaand.es>
2026-01-19 17:26:48 +0100

    instalacion de shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      probando movida tailwind
+      <Button>Button</Button>
+
+    </div>
+  )
+}
+
+export default App
