d3f4ca447eb4051e1935ef8396c5e25ddf76d022
jruifri583 <jruifri583@g.educaand.es>
2026-01-19 18:29:26 +0100

    creacion de algunos componentes de front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button"
+import { CardLogin } from "@/components/ui/CardLogin"
+import { Label } from "@/components/ui/Label"
+import { Input } from "@/components/ui/Input"
+
 
 const App = () => {
   return (
     <div>
-      probando movida tailwind
-      <Button>Button</Button>
+      <CardLogin>
+        <Label htmlFor="email">Email*</Label>
+        <Input id="email" type="email" placeholder="Enter your email" />
+        <Label htmlFor="password">Contraseña*</Label>
+        <Input id="password" type="password" placeholder="Crear una contraseña" />
+        <Label htmlFor="password">Repite contraseña*</Label>
+        <Input id="password" type="password" placeholder="Confirmar contraseña" />
+        <Button>Registrarse</Button>
+        <Button variant="outline">Registrarse con Google</Button>
+      </CardLogin>
 
     </div>
   )
+
+  
 }
+
 
 export default App
