271399b04791274e3ded64896b28408ad001c107
jruifri583 <jruifri583@g.educaand.es>
2026-01-21 12:11:41 +0100

    feat: añadir login y registro Google

--- backend/app/Http/Controllers/Api/GoogleAuthController.php
+++ backend/app/Http/Controllers/Api/GoogleAuthController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers\Api;
-
 use App\Http\Controllers\Controller;
 use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import api from "@/lib/axios";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { CardSinBorde, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import axios from "axios";
 
@@ -27,9 +28,23 @@ export default function Register() {
     }
   };
 
+  useEffect(() => {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("token");
+  if (token) {
+    localStorage.setItem("token", token);
+    window.history.replaceState({}, document.title, "/");
+    navigate("/dashboard"); // redirige al dashboard
+  }
+}, [navigate]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <Card className="w-full max-w-2xl">
+    <div className="grid grid-cols-2 min-h-screen w-full ">
+      <div className="bg-primary bg-[url('/logo.png')] bg-no-repeat bg-center" >
+      </div>
+      <div className="flex flex-col items-center justify-center p-8">
+        <div className="bg-[url('/logoLinea.png')] bg-no-repeat bg-center bg-contain w-100 h-40 mb-6" ></div>
+     <CardSinBorde className="w-full max-w-md mx-auto" >
         <CardHeader><CardTitle className="text-2xl text-center">Crear Cuenta</CardTitle></CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit} className="mt-8 space-y-6">
@@ -46,9 +61,21 @@ export default function Register() {
               <Input type="password" onChange={e => setFormData({...formData, password_confirmation: e.target.value})} />
             </div>
             <Button type="submit" className="md:col-span-2 w-full mt-4">Registrarse</Button>
+            <Button
+            type="button"
+              onClick={() => window.location.href = "http://localhost:8000/auth/google"}
+                className="w-full mt-4 bg-white border border-gray-300 text-gray-800 hover:bg-gray-100 flex items-center justify-center gap-2"
+                  >
+                <div className="bg-[url('/google.png')] w-6 h-6 bg-center bg-contain bg-no-repeat" />
+                  Registrarse con Google
+                </Button>
           </form>
         </CardContent>
-      </Card>
+      </CardSinBorde> 
+      </div>
     </div>
+      
+
   );
+
 }
