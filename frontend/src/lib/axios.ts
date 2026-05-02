ce89a53f10f37d096fca1cdfdaef40c0220ecab4
jruifri583 <jruifri583@g.educaand.es>
2026-05-02 16:15:44 +0200

    axios

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+const getBaseURL = () => {
+  if (import.meta.env.VITE_API_URL) {
+    return `${import.meta.env.VITE_API_URL}/api`;
+  }
+  if (window.location.hostname !== "localhost") {
+    return `${window.location.origin}/api`;
+  }
+  return "http://localhost:8000/api";
+};
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : "http://localhost:8000/api",
+  baseURL: getBaseURL(),
   headers: {
     Accept: "application/json",
   },
